fbba45fee5b0307a05b1efe169510d57aff86202
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-09-30 00:34:55 +0100

    add last task

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ *infinite_while - infinite loop
+ */
+ 
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ *main - main function
+ */
+ 
+int main() {
+	int i = 0;
+	pid_t child_pid;
+
+	for (; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == 0) {
+		printf("Zombie process created, PID: %d\n", getpid());
+		exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
