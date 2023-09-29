a84d72f0059312634bb60ad590a68e8498b5faf8
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-09-30 00:42:50 +0100

    add

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -5,8 +5,9 @@
 
 /**
  *infinite_while - infinite loop
+ *Return: 0 for success
  */
- 
+
 int infinite_while(void)
 {
 	while (1)
@@ -18,16 +19,19 @@ int infinite_while(void)
 
 /**
  *main - main function
+ *Return: 0 for success
  */
- 
-int main() {
+
+int main(void)
+{
 	int i = 0;
 	pid_t child_pid;
 
 	for (; i < 5; i++)
 	{
 		child_pid = fork();
-		if (child_pid == 0) {
+		if (child_pid == 0)
+		{
 		printf("Zombie process created, PID: %d\n", getpid());
 		exit(0);
 		}
