664e074f2000ae581caff4b8627e1029aca55d1e
Yeeloman <yami.onlyme@gmail.com>
2024-01-17 02:53:03 +0100

    test

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,27 +1,34 @@
-# §/usr/bin/python3
-"""recursive function"""
+#!/usr/bin/python3
+"""
+0x16. API advanced
+"""
 
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """return the titles of the hot articles recursively
-    subreddit is the target
-     Args:
-        subreddit (str): subreddit
+    """A recursive function that queries the Reddit
+    API and returns a list containing the titles of all
+    hot articles for a given subreddit. If no results
+    are found for the given subreddit, the function should
+    return None."""
+    if not subreddit or not isinstance(subreddit, str):
+        return None
+    params = {"after": after} if after else {}
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "costum"},
+        params=params,
+    )
 
-    Returns:
-        int: number of subscribers
-        """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'user-agent': 'custom'}
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code == 200:
-        r = r.json()
-        for post in r.get('data').get('children'):
-            hot_list.append(post.get('data').get('title'))
-        if r.get('data').get('after'):
-            recurse(subreddit, hot_list)
-        return hot_list
+    if response.status_code == 200:
+        if response.json()["data"]:
+            data = response.json()["data"]
+            for item in data["children"]:
+                hot_list.append(item["data"]["title"])
+            if data["after"]:
+                return recurse(subreddit, hot_list, after=data["after"])
+            else:
+                return hot_list
     else:
         return None
