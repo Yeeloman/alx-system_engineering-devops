75bac93fe095b08e6a973ac2633b9a1cb11eebfe
Yeeloman <yami.onlyme@gmail.com>
2024-01-17 14:34:11 +0100

    add task 0
    the issue was in extension inside a file
    phpp instead of php

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# problem was due to misspelled phpp so i changed it to php `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
